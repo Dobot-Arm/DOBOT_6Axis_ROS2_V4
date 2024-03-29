b9bc2543384554ac53a188838fbf4203d0d84ff2
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-03-29 19:27:18 +0800

    Add files via upload

---
+++ cr12_moveit/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("cr12_robot", package_name="cr12_moveit").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
---
+++ cr5_moveit/launch/warehouse_db.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_warehouse_db_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("cr5_robot", package_name="cr5_moveit").to_moveit_configs()
+    return generate_warehouse_db_launch(moveit_config)
