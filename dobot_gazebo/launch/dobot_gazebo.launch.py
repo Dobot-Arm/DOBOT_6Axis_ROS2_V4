d39d08ead69ffbe5a384231203335db3a811d5b7
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-04-12 14:29:20 +0800

    Add files via upload

---
+++ dobot_gazebo/launch/dobot_gazebo.launch.py
@@ -0,0 +1,70 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+import yaml
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        return None
+
+
+def generate_launch_description():
+    mane = os.getenv("DOBOT_TYPE")
+    robot_name_in_model = f'{mane}_robot'
+    package_name = 'cra_description'
+    urdf_name = f"{mane}_robot.xacro"
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                launch_arguments={}.items(),
+             )
+    cra_description_path = os.path.join(
+        get_package_share_directory(package_name))
+
+    xacro_file = os.path.join(cra_description_path,
+                              'urdf',
+                              urdf_name)
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description_config = doc.toxml()
+    robot_description = {'robot_description': robot_description_config}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', robot_name_in_model],
+                        output='screen')
+    
+    return LaunchDescription([
+      gazebo,
+      node_robot_state_publisher,
+      spawn_entity
+    ])
