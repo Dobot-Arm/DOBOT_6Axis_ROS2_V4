6e14e345f127fa267d7daef86957e1db94c46296
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-04-24 18:22:27 +0800

    Update demo.py

--- dobot_demo/dobot_demo/demo.py
+++ dobot_demo/dobot_demo/demo.py
@@ -14,7 +14,7 @@ class adderClient(Node):
         self.MovJ_l = self.create_client(MovJ,'/dobot_bringup_ros2/srv/MovJ')
         self.SpeedFactor_l = self.create_client(SpeedFactor,'/dobot_bringup_ros2/srv/SpeedFactor')
         self.MovL_l = self.create_client(MovL,'/dobot_bringup_ros2/srv/MovL')
-        self.DO_l = self.create_client(DO,'/dobot_bringup_ros2/srv/DO', )
+        self.DO_l = self.create_client(DO,'/dobot_bringup_ros2/srv/DO' )
         while not self.EnableRobot_l.wait_for_service(timeout_sec=1.0):                  # 循环等待服务器端成功启动
             self.get_logger().info('service not available, waiting again...') 
                     
@@ -29,35 +29,28 @@ class adderClient(Node):
     def point(self, Move, X_j1, Y_j2, Z_j3, RX_j4, RY_j5, RZ_j6):  # 运动指令
         if Move == "MovJ":
             P1 = MovJ.Request()
-            P1.mode = 0
-            P1.x = X_j1
-            P1.y = Y_j2
-            P1.z = Z_j3
-            P1.a = RX_j4
-            P1.b = RY_j5
-            P1.c = RZ_j6
+            P1.mode = False
+            P1.a = float(X_j1)
+            P1.b = float(Y_j2)
+            P1.c = float(Z_j3)
+            P1.d = float(RX_j4)
+            P1.e = float(RY_j5)
+            P1.f = float(RZ_j6)
             response = self.MovJ_l.call_async(P1)
             print(response)
         elif Move == "MovL":
             P1 = MovL.Request()
-            P1.mode = 0
-            P1.x = X_j1
-            P1.y = Y_j2
-            P1.z = Z_j3
-            P1.a = RX_j4
-            P1.b = RY_j5
-            P1.c = RZ_j6
+            P1.mode = False
+            P1.a = float(X_j1)
+            P1.b = float(Y_j2)
+            P1.c = float(Z_j3)
+            P1.d = float(RX_j4)
+            P1.e = float(RY_j5)
+            P1.f = float(RZ_j6)
             response = self.MovL_l.call_async(P1)
             print(response)
         else:
             print("无该指令")
-
-    def DO(self, index, status):  # IO 控制夹爪/气泵
-        DO_V = DO.Request()
-        DO_V.index = index
-        DO_V.status = status
-        response = self.DO_l.call_async(DO_V)
-        print(response)
 
 
 def main(args=None):
