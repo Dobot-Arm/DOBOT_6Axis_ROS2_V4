bd2f12959f77b72f6a4677df153eac295532a5bd
brawn <safar@mowito.in>
2024-05-21 22:39:59 +0530

    Sv -- removed duplicate addition

--- dobot_bringup_v4/src/main.cpp
+++ dobot_bringup_v4/src/main.cpp
@@ -34,11 +34,7 @@ int main(int argc, char *argv[])
   std::string a = robot_type == nullptr ? "cr5" : robot_type;
   std::string b = "_robot/joint_controller/follow_joint_trajectory";
   std::string ss = z + a + b;
-  for (uint32_t i = 0; i < 6; i++)
-  {
-    joint_state_msg.position.push_back(0.0);
-    joint_state_msg.name.push_back(std::string("joint") + std::to_string(i + 1));
-  }
+ 
 
   double rate_value = robot->declare_parameter<double>("JointStatePublishRate", 10.0);
 
