09d3c9e0d385f8dbae88d5839d5d21c2bea4ea80
lingmeng <yumo4961@gmail.com>
2025-07-01 18:43:11 +0800

    dobot_demo

--- dobot_demo/setup.py
+++ dobot_demo/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='dobot',
-    maintainer_email='',
+    maintainer_email='dobot@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
